2c52456506e77131d7293330f38feaeea6feddb8
mathigatti <mathigatti@gmail.com>
2021-04-06 15:31:52 -0300

    visualizer

--- visualizer/script.js
+++ visualizer/script.js
@@ -2,9 +2,8 @@ let octahedron;
 
 var zoom = 9.00;
 var zMin = 0.05;
-var zMax = 20.00;
-var sensativity = 0.005;
-
+var zMax = 500.00;
+var sensativity = 4;
 
 function preload() {
   octahedron = loadModel('cellular_form.obj');
@@ -21,8 +20,12 @@ function draw() {
   scale(zoom);
   model(octahedron);
 }
- 
-function mouseWheel(event) {
-  zoom += sensativity * event.delta;
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+	zoom -= sensativity;
+  } else if (keyCode === RIGHT_ARROW) {
+	zoom += sensativity;
+  }
   zoom = constrain(zoom, zMin, zMax);
 }
